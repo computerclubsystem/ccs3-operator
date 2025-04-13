8e56840e4f9d7de48fa9163906a434b957484331
test <test@test.test>
2025-04-13 11:19:45 +0300

    Reduce connectivity information in device details #76

--- projects/messages/src/lib/entities/device-connectivity-item.ts
+++ projects/messages/src/lib/entities/device-connectivity-item.ts
@@ -1,10 +1,4 @@
 export interface DeviceConnectivityItem {
   deviceId?: number | null;
-  deviceName?: string | null;
-  certificateThumbprint: string;
-  connectionsCount: number;
-  secondsSinceLastConnected: number;
-  messagesCount: number;
-  secondsSinceLastMessage?: number | null;
   isConnected: boolean;
 }
